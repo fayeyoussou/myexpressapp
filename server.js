8e5985d06c9b5ea4a7ff7ed907469fa07c453026
fayeyoussou <fayeyoussoupha@gmail.com>
2024-05-14 12:11:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,38 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const nodemailer = require("nodemailer");
+var token = "";
+
 require("dotenv").config();
 mongoose.connect("mongodb://mongo:27017/tokenDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+async function getToken() {
+  const authString = Buffer.from(
+    `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
+  ).toString("base64");
+  try {
+    const response = await fetch("https://oauth.battle.net/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${authString}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials",
+    });
 
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    token = data.access_token;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 const transporter = nodemailer.createTransport({
   service: "gmail", // For Gmail, use 'gmail'
   auth: {
@@ -48,12 +74,21 @@ const Token = mongoose.model("Token", tokenSchema);
 app.post("/fetch-token", async (req, res) => {
   const apiUrl = "https://eu.api.blizzard.com/data/wow/token/index";
   try {
+    const now = new Date();
+    const isTokenEmpty = token.length == 0;
+    const isLateHour = now.getHours() == 11 || now.getHours() == 23;
+    const isLateMinute = now.getMinutes() >= 30;
+    if (isTokenEmpty || (isLateMinute && isLateHour)) {
+      console.log(
+        "Condition met: Token is empty or the current time is 11:30-11:59 AM/PM"
+      );
+      await getToken();
+    }
     const response = await axios.get(apiUrl, {
-      headers: { Authorization: `Bearer ${process.env.BATTLE_NET_TOKEN}` },
+      headers: { Authorization: `Bearer ${token}` },
       params: { namespace: "dynamic-eu", locale: "en_US" },
     });
     const newToken = new Token({ price: response.data.price });
-    const now = new Date();
     const thirtyDaysAgo = new Date(now);
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
@@ -97,21 +132,22 @@ app.get("/tokens/monthly", async (req, res) => {
     const now = new Date();
     const thirtyDaysAgo = new Date(now);
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
-    
+
     const monthlyTokens = await Token.find({
       timestamp: { $gte: thirtyDaysAgo, $lt: now },
     });
-    
+
     if (monthlyTokens.length === 0) {
       res.json({ message: "No token data available for the last 30 days." });
       return;
     }
-    
-    const prices = monthlyTokens.map(token => token.price);
+
+    const prices = monthlyTokens.map((token) => token.price);
     const minPrice = Math.min(...prices);
     const maxPrice = Math.max(...prices);
-    const meanPrice = prices.reduce((acc, price) => acc + price, 0) / prices.length;
-    
+    const meanPrice =
+      prices.reduce((acc, price) => acc + price, 0) / prices.length;
+
     res.json({
       minPrice: minPrice,
       maxPrice: maxPrice,
@@ -128,21 +164,22 @@ app.get("/tokens/weekly", async (req, res) => {
     const now = new Date();
     const sevenDaysAgo = new Date(now);
     sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-    
+
     const weeklyTokens = await Token.find({
       timestamp: { $gte: sevenDaysAgo, $lt: now },
     });
-    
+
     if (weeklyTokens.length === 0) {
       res.json({ message: "No token data available for the last 7 days." });
       return;
     }
-    
-    const prices = weeklyTokens.map(token => token.price);
+
+    const prices = weeklyTokens.map((token) => token.price);
     const minPrice = Math.min(...prices);
     const maxPrice = Math.max(...prices);
-    const meanPrice = prices.reduce((acc, price) => acc + price, 0) / prices.length;
-    
+    const meanPrice =
+      prices.reduce((acc, price) => acc + price, 0) / prices.length;
+
     res.json({
       minPrice: minPrice,
       maxPrice: maxPrice,
